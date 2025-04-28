4cd5dedddb6fa4cb8a8c09c55531c36ee8828eaa
Thalisson <thalisson_21@icloud.com>
2025-04-28 15:04:35 -0400

    fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import LeaveMessagePage from "./pages/LeaveMessagePage";
-import Celebration from "./components/Celebration"; // Importa direto
+
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/mensagem" element={<LeaveMessagePage />} />
-        <Route path="/celebration" element={<Celebration />} /> 
+      
       </Routes>
     </Router>
   );
