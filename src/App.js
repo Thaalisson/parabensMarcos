60379f55cc8c09c8a8d881dd4265b2cc69b9a584
Thalisson <thalisson_21@icloud.com>
2025-04-28 13:03:25 -0400

    test

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import Celebration from "./components/Celebration"; // Importa direto
 function App() {
   return (
     <Router>
+
+          {/* 🔥 Dummy Hidden Form para o Netlify detectar no build */}
+       <form name="marcos-messages" data-netlify="true" hidden>
+        <input type="text" name="name" />
+        <textarea name="message"></textarea>
+        <input type="file" name="photo" />
+      </form>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/mensagem" element={<LeaveMessagePage />} />
